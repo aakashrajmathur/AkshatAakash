1d0504432b1c696f26c40d5dd2a75c209e3efc74
Aakash Mathur <aakashrajmathur@gmail.com>
2017-06-23 16:40:05 -0500

    First commit. Skeletal structure uploaded.

---
+++ FamilyFeud/FamilyFeud/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FamilyFeud.Controllers
+{
+    public class HomeController : Controller
+    {
+        private string filePath = @"wwwroot\data.txt"; //@"C:\Users\aamathur\Desktop\clutter\livebrackets\data.txt";
+
+        public IActionResult Index()
+        {
+            ViewData["DataTransfer"] = System.IO.File.ReadAllLines(filePath);
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string valueINeed)
+        {
+            System.IO.File.AppendAllText(filePath, valueINeed + "\n");
+            ViewData["DataTransfer"] = System.IO.File.ReadAllLines(filePath);
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
